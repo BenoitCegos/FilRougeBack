aaa4a5089e58dc3cc366b8ae5a5b9ee52255ae15
BenoitCegos <benoit.dufour@free.fr>
2024-06-25 11:25:18 +0200

    db3

---
+++ Controllers/ProjetController.cs
@@ -0,0 +1,71 @@
+﻿using FilRouge.DAO;
+using FilRouge.Models;
+using Microsoft.AspNetCore.Mvc;
+//using FilRouge.Controllers.DAO;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace FilRouge.Controllers
+{
+    public class ProjetController : Controller
+    {
+        private readonly ProjetDAO _DAO;
+
+        public ProjetController(ProjetDAO dao)
+        {
+            _DAO = dao;
+        }
+
+        
+
+
+
+        [HttpGet("projets")]
+        public async Task<ActionResult<IEnumerable<Projet>>> GetProjets()
+        {
+            var reponse = await _DAO.GetProjets();
+            return Ok(reponse);
+        }
+
+        [HttpGet("projets/{id}")]
+        public async Task<ActionResult<Projet>> GetProjet(int id)
+        {
+            var Projet = await _DAO.GetProjet(id);
+            return Ok(Projet);
+
+        }
+
+        [HttpPost("projets")]
+        public async Task<ActionResult<Projet>> PostProjet(Projet Projet)
+        {
+            await _DAO.AddProjet(Projet);
+            return CreatedAtAction("GetProjet", new { id = Projet.Id }, Projet);
+        }
+
+        [HttpPut("/projets/{id}")]
+        public async Task<IActionResult> PutProjet(int id, Projet Projet)
+        {
+            if (id != Projet.Id)
+            {
+                return BadRequest();
+            }
+
+            await _DAO.UpdateProjet(Projet);
+
+            return NoContent();
+        }
+
+        [HttpDelete("/projets/{id}")]
+        public async Task<IActionResult> DeleteProjet(int id)
+        {
+            var success = await _DAO.DeleteProjet(id);
+            if (!success)
+            {
+                return NotFound();
+            }
+
+            return NoContent();
+        }
+
+    }
+}
---
+++ DAO/ProjetDAO.cs
@@ -0,0 +1,89 @@
+﻿using FilRouge.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FilRouge.DAO
+{
+    public class ProjetDAO
+    {
+        private readonly FilRougeContext _DB;
+
+        public ProjetDAO(FilRougeContext context)
+        {
+            _DB = context;
+        }
+
+        public async Task<IEnumerable<Projet>> GetProjets()
+        {
+            return await _DB.Projets.ToListAsync();
+        }
+
+        public async Task<Projet> GetProjet(int id)
+        {
+            return await _DB.Projets.FindAsync(id);
+        }
+
+        public async Task<Projet> AddProjet(Projet Projet)
+        {
+            _DB.Projets.Add(Projet);
+            await _DB.SaveChangesAsync();
+            return Projet;
+        }
+
+        public async Task<Projet> UpdateProjet(Projet Projet)
+        {
+            _DB.Entry(Projet).State = EntityState.Modified;
+            await _DB.SaveChangesAsync();
+            return Projet;
+        }
+
+        public async Task<bool> DeleteProjet(int id)
+        {
+            var Projet = await _DB.Projets.FindAsync(id);
+            if (Projet == null) return false;
+
+            _DB.Projets.Remove(Projet);
+            await _DB.SaveChangesAsync();
+            return true;
+        }
+        /*
+        public async Task<IEnumerable<User>> GetUsers()
+        {
+            return await _context.Users.Include(u => u.Purchases)
+                                       .ThenInclude(p => p.PurchaseProjets)
+                                       .ThenInclude(pa => pa.Projet)
+                                       .ToListAsync();
+        }
+
+        public async Task<User> GetUser(int id)
+        {
+            return await _context.Users.Include(u => u.Purchases)
+                                       .ThenInclude(p => p.PurchaseProjets)
+                                       .ThenInclude(pa => pa.Projet)
+                                       .FirstOrDefaultAsync(u => u.UserId == id);
+        }
+
+        public async Task<User> AddUser(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+            return user;
+        }
+
+        public async Task<User> UpdateUser(User user)
+        {
+            _context.Entry(user).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return user;
+        }
+
+        public async Task<bool> DeleteUser(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            if (user == null) return false;
+
+            _context.Users.Remove(user);
+            await _context.SaveChangesAsync();
+            return true;
+        }*/
+    }
+}
